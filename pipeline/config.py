6e4dff71c9fbefa3c44fa595acc0f0ee145c20fb
Mathias Aloui <mathias.aloui@gmail.com>
2022-04-21 21:37:24 +0000

    change config

--- pipeline/config.py
+++ pipeline/config.py
@@ -3,7 +3,7 @@ from os import environ
 
 environ["CUDA_VISIBLE_DEVICES"] = "-1"
 environ["EMBEDDING"] = "128"
-environ["ENV_PATH"] = "D:/Programmes/anaconda/envs/pipeline/Lib/site-packages"
+environ["ENV_PATH"] = "/home/data/.conda/envs/pipeline/lib/python3.8/site-packages"
 environ["INPUT_SIZE"] = "224"
 environ["PROFIL_SIZE"] = "224"
 environ["PROFIL_WEIGHTS"] = "profil/weights/weights.h5"
